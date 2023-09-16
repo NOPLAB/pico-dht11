516526e241759b93d35231ec75ad59925d1b2c6d
nop <noplab90@gmail.com>
2023-09-16 18:21:00 +0900

    update serial

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,70 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
-use defmt::*;
-use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
-use panic_probe as _;
+use pac::interrupt;
+use rp_pico::entry;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
+use panic_halt as _;
+// use panic_probe as _;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+use rp_pico::hal;
+use rp_pico::hal::pac;
+
+use rp_pico::Pins;
+
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
+
+// USB Communications Class Device support
+use usbd_serial::SerialPort;
+
+use hal::{clocks::Clock, sio::Sio};
+
+use dht_sensor::*;
+
+// Used to demonstrate writing formatted strings
+use core::fmt::Write;
+use heapless::String;
+
+static mut USB_DEV: Option<UsbDevice<hal::usb::UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+
+fn wait_until_timeout<E, F>(
+    delay: &mut dyn Delay,
+    func: F,
+    timeout_us: u8,
+) -> Result<(), DhtError<E>>
+where
+    F: Fn() -> Result<bool, E>,
+{
+    for _ in 0..timeout_us {
+        if func()? {
+            return Ok(());
+        }
+        delay.delay_us(1_u8);
+    }
+    Err(DhtError::Timeout)
+}
 
 #[entry]
 fn main() -> ! {
-    info!("Program start");
+    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
+
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -44,30 +75,86 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    unsafe { USB_BUS = Some(usb_bus) };
+
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
+
+    // Set up the USB Communications Class Device driver
+    let usb_serial = SerialPort::new(bus_ref);
+
+    // Create a USB device with a fake VID and PID
+    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    unsafe { USB_SERIAL = Some(usb_serial) }
+
+    unsafe { USB_DEV = Some(usb_dev) }
+
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
+
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    let pins = Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut dht11_pin = hal::gpio::InOutPin::new(pins.gpio28);
+    let _ = dht11_pin.set_high();
+
+    // watchdog.start(1.secs());
+
+    delay.delay_ms(1000);
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
+        // todo ここでTimeoutエラーが出る
+        match dht11::Reading::read(&mut delay, &mut dht11_pin) {
+            Ok(measurement) => {
+                let mut text: String<128> = String::new();
+                writeln!(
+                    &mut text,
+                    "Temp: {} Hum: {}\r\n",
+                    measurement.temperature, measurement.relative_humidity
+                )
+                .unwrap();
+                if let Some(serial_ref) = unsafe { USB_SERIAL.as_mut() } {
+                    let _ = serial_ref.write(text.as_bytes());
+                }
+            }
+            Err(err) => {
+                let mut text: String<128> = String::new();
+                writeln!(&mut text, "Error: {:?}\r\n", err).unwrap();
+                if let Some(serial_ref) = unsafe { USB_SERIAL.as_mut() } {
+                    let _ = serial_ref.write(text.as_bytes());
+                }
+            }
+        }
         delay.delay_ms(500);
     }
 }
 
-// End of file
+#[interrupt]
+fn USBCTRL_IRQ() {
+    if let Some(dev_ref) = unsafe { USB_DEV.as_mut() } {
+        if let Some(serial_ref) = unsafe { USB_SERIAL.as_mut() } {
+            let _ = dev_ref.poll(&mut [serial_ref]);
+        }
+    }
+}
